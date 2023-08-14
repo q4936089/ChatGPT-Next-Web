79d6b7bbabc1073c077026d9645ee063f46ff0c4
q4936089 <40992957+q4936089@users.noreply.github.com>
2023-08-14 20:55:36 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,9 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
-              <IconButton icon={<GithubIcon />} shadow />
-            </a>
+            
           </div>
         </div>
         <div>
